73efd4cb6ae3a3bc348791a6d0949471665577ba
yassinemontassar <sawthegamer70@gmail.com>
2023-12-10 12:32:49 +0100

    mobile device update

--- app/(dashboard)/builder/[id]/layout.tsx
+++ app/(dashboard)/builder/[id]/layout.tsx
@@ -1,7 +1,35 @@
-import React, { ReactNode } from "react";
+"use client";
+import React, { ReactNode, useEffect, useState } from "react";
 
-function layout({ children }: { children: ReactNode }) {
-  return <div className="flex w-full flex-grow mx-auto">{children}</div>;
+function Layout({ children }: { children: ReactNode }) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // You can adjust the threshold as needed
+    };
+
+    // Initial check on mount
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Remove event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div className={`flex w-full flex-grow mx-auto ${isMobile ? 'mobile-message' : ''}`}>
+      {isMobile ? (
+        <p>You cant access this page on a mobile device.</p>
+      ) : (
+        children
+      )}
+    </div>
+  );
 }
 
-export default layout;
+export default Layout;
